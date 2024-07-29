7597b4a18778ddfcb1518dea7212d0a229d424df
Mark Wassenaar <mark.wassenaar@live.nl>
2024-07-29 16:44:46 +0200

    update

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,9 @@
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
-// import { TailwindProvider } from "tailwindcss-react-native";
 import { Link } from "expo-router";
 
 const App = () => {
   return (
-    // <TailwindProvider>
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-2xl">Maplenator</Text>
       <StatusBar style="auto" />
@@ -13,7 +11,6 @@ const App = () => {
         Go to Profile
       </Link>
     </View>
-    // </TailwindProvider>
   );
 };
 
